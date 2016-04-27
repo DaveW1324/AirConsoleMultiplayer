7d9ef0105f0b9d8a77914add212a79ccaf7d98a0
Scott Schmitz <scottaschmitz@gmail.com>
2016-04-27 19:17:27 -0400

    extremely basic communication between controller and game

---
+++ Assets/Game/ConnectionManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class ConnectionManager : MonoBehaviour {
+
+	public void Awake() {
+		AirConsole.instance.onMessage += OnMessage;
+		AirConsole.instance.onConnect += OnConnect;
+		AirConsole.instance.onDisconnect += OnDisconnect;
+	}
+
+	/// <summary>
+	/// Raises the connect event.
+	/// </summary>
+	/// <param name="deviceId">Device identifier.</param>
+	public void OnConnect (int deviceId)
+	{
+		Debug.LogWarning ("Player Connected: " + deviceId);
+		if (AirConsole.instance.GetActivePlayerDeviceIds.Count < 4) {
+			AirConsole.instance.SetActivePlayers (4);
+		} else {
+			Debug.LogWarning ("Too many players");
+		}			
+	}
+
+	/// <summary>
+	/// If the game is running and one of our active players leaves we should do something
+	/// </summary>
+	/// <param name="deviceId">Device identifier.</param>
+	public void OnDisconnect (int deviceId)
+	{
+		Debug.LogWarning ("OnDisconnect");
+
+		int activePlayer = AirConsole.instance.ConvertDeviceIdToPlayerNumber (deviceId);
+
+		if (activePlayer != -1) {
+			Debug.LogWarning ("Player Disconnected: " + activePlayer);
+		}
+	}
+
+
+	/// <summary>
+	/// Check which of the players has sent the message
+	/// </summary>
+	/// <param name="deviceId">DeviceId.</param>
+	/// <param name="data">Data.</param>
+	void OnMessage (int deviceId, JToken data) {
+		int activePlayer = AirConsole.instance.ConvertDeviceIdToPlayerNumber (deviceId);
+
+		if (activePlayer != -1) {
+			Debug.LogWarning ("Successfully recieved message from: " + activePlayer);
+		}
+	}
+}
